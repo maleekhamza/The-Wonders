e7d452c06c9c330639fe9bbb70c27b566ade0e06
labidimalek <melek.islem105@gmail.com>
2021-04-21 22:25:05 +0100

    migration+model(table club & table resp_medias

---
+++ database/migrations/2021_04_21_210519_create_clubs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom_club');
+            $table->integer('nb_membres');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
