8d984b3627d02780164386b68bf4a8b4b6e67134
dali009 <dalifatnassi20@gmail.com>
2021-05-01 21:24:18 +0100

    pull last version from master

--- database/seeds/SallesTableSeeder.php
+++ database/seeds/SallesTableSeeder.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Seeder;
 class SallesTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds
      *
      * @return void
      */
     public function run()
     {
         factory(Salle::class, 10)->create();
-        
     }
 }
